b5c3eda9d83cfbcc7c08b5423b1afad9a6d92bcd
luis.teixeira <luisedteixeira@gmail.com>
2025-02-23 10:25:50 -0300

    Add database credentials

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('summerbikes', 'root', 'p1cad1nh0', {
-  host: 'localhost',
+const sequelize = new Sequelize('summerbikes', 'summerbikes', 'p1cad1nh0', {
+  host: 'mysql.summerbikes.kinghost.net',
   dialect: 'mysql'
 });
 
